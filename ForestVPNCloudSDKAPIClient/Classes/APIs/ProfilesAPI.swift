6d544d7877ccc81695388f172d09570d9dbc1adc
George Kutsurua <g.kutsurua@gmail.com>
2022-07-21 15:04:48 +0400

    device info

--- ForestVPNCloudSDKAPIClient/Classes/APIs/ProfilesAPI.swift
+++ ForestVPNCloudSDKAPIClient/Classes/APIs/ProfilesAPI.swift
@@ -13,6 +13,57 @@ import AnyCodable
 open class ProfilesAPI {
 
     /**
+     Attach device info
+     
+     - parameter profileId: (path)  
+     - parameter deviceInfo: (body)  (optional)
+     - parameter apiResponseQueue: The queue on which api response is dispatched.
+     - parameter completion: completion handler to receive the data and the error objects
+     */
+    @discardableResult
+    open class func attachDeviceInfo(profileId: UUID, deviceInfo: DeviceInfo? = nil, apiResponseQueue: DispatchQueue = ForestVPNCloudSDKAPIAPI.apiResponseQueue, completion: @escaping ((_ data: String?, _ error: Error?) -> Void)) -> RequestTask {
+        return attachDeviceInfoWithRequestBuilder(profileId: profileId, deviceInfo: deviceInfo).execute(apiResponseQueue) { result in
+            switch result {
+            case let .success(response):
+                completion(response.body, nil)
+            case let .failure(error):
+                completion(nil, error)
+            }
+        }
+    }
+
+    /**
+     Attach device info
+     - POST /profiles/{profileId}/device/info/
+     - BASIC:
+       - type: http
+       - name: BearerTokenAuth
+     - parameter profileId: (path)  
+     - parameter deviceInfo: (body)  (optional)
+     - returns: RequestBuilder<String> 
+     */
+    open class func attachDeviceInfoWithRequestBuilder(profileId: UUID, deviceInfo: DeviceInfo? = nil) -> RequestBuilder<String> {
+        var localVariablePath = "/profiles/{profileId}/device/info/"
+        let profileIdPreEscape = "\(APIHelper.mapValueToPathItem(profileId))"
+        let profileIdPostEscape = profileIdPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+        localVariablePath = localVariablePath.replacingOccurrences(of: "{profileId}", with: profileIdPostEscape, options: .literal, range: nil)
+        let localVariableURLString = ForestVPNCloudSDKAPIAPI.basePath + localVariablePath
+        let localVariableParameters = JSONEncodingHelper.encodingParameters(forEncodableObject: deviceInfo)
+
+        let localVariableUrlComponents = URLComponents(string: localVariableURLString)
+
+        let localVariableNillableHeaders: [String: Any?] = [
+            :
+        ]
+
+        let localVariableHeaderParameters = APIHelper.rejectNilHeaders(localVariableNillableHeaders)
+
+        let localVariableRequestBuilder: RequestBuilder<String>.Type = ForestVPNCloudSDKAPIAPI.requestBuilderFactory.getBuilder()
+
+        return localVariableRequestBuilder.init(method: "POST", URLString: (localVariableUrlComponents?.string ?? localVariableURLString), parameters: localVariableParameters, headers: localVariableHeaderParameters)
+    }
+
+    /**
      Profile details
      
      - parameter profileId: (path)  
@@ -65,13 +116,13 @@ open class ProfilesAPI {
      Profile protocol config
      
      - parameter profileId: (path)  
-     - parameter protocolId: (path)  
+     - parameter protocolSlug: (path)  
      - parameter apiResponseQueue: The queue on which api response is dispatched.
      - parameter completion: completion handler to receive the data and the error objects
      */
     @discardableResult
-    open class func getProfileProtocolConfig(profileId: UUID, protocolId: UUID, apiResponseQueue: DispatchQueue = ForestVPNCloudSDKAPIAPI.apiResponseQueue, completion: @escaping ((_ data: String?, _ error: Error?) -> Void)) -> RequestTask {
-        return getProfileProtocolConfigWithRequestBuilder(profileId: profileId, protocolId: protocolId).execute(apiResponseQueue) { result in
+    open class func getProfileProtocolConfig(profileId: UUID, protocolSlug: UUID, apiResponseQueue: DispatchQueue = ForestVPNCloudSDKAPIAPI.apiResponseQueue, completion: @escaping ((_ data: String?, _ error: Error?) -> Void)) -> RequestTask {
+        return getProfileProtocolConfigWithRequestBuilder(profileId: profileId, protocolSlug: protocolSlug).execute(apiResponseQueue) { result in
             switch result {
             case let .success(response):
                 completion(response.body, nil)
@@ -83,22 +134,22 @@ open class ProfilesAPI {
 
     /**
      Profile protocol config
-     - GET /profiles/{profileId}/protocols/{protocolId}.conf
+     - GET /profiles/{profileId}/protocols/{protocolSlug}.conf
      - BASIC:
        - type: http
        - name: BearerTokenAuth
      - parameter profileId: (path)  
-     - parameter protocolId: (path)  
+     - parameter protocolSlug: (path)  
      - returns: RequestBuilder<String> 
      */
-    open class func getProfileProtocolConfigWithRequestBuilder(profileId: UUID, protocolId: UUID) -> RequestBuilder<String> {
-        var localVariablePath = "/profiles/{profileId}/protocols/{protocolId}.conf"
+    open class func getProfileProtocolConfigWithRequestBuilder(profileId: UUID, protocolSlug: UUID) -> RequestBuilder<String> {
+        var localVariablePath = "/profiles/{profileId}/protocols/{protocolSlug}.conf"
         let profileIdPreEscape = "\(APIHelper.mapValueToPathItem(profileId))"
         let profileIdPostEscape = profileIdPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
         localVariablePath = localVariablePath.replacingOccurrences(of: "{profileId}", with: profileIdPostEscape, options: .literal, range: nil)
-        let protocolIdPreEscape = "\(APIHelper.mapValueToPathItem(protocolId))"
-        let protocolIdPostEscape = protocolIdPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
-        localVariablePath = localVariablePath.replacingOccurrences(of: "{protocolId}", with: protocolIdPostEscape, options: .literal, range: nil)
+        let protocolSlugPreEscape = "\(APIHelper.mapValueToPathItem(protocolSlug))"
+        let protocolSlugPostEscape = protocolSlugPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+        localVariablePath = localVariablePath.replacingOccurrences(of: "{protocolSlug}", with: protocolSlugPostEscape, options: .literal, range: nil)
         let localVariableURLString = ForestVPNCloudSDKAPIAPI.basePath + localVariablePath
         let localVariableParameters: [String: Any]? = nil
 
@@ -162,5 +213,59 @@ open class ProfilesAPI {
         let localVariableRequestBuilder: RequestBuilder<[ModelProtocol]>.Type = ForestVPNCloudSDKAPIAPI.requestBuilderFactory.getBuilder()
 
         return localVariableRequestBuilder.init(method: "GET", URLString: (localVariableUrlComponents?.string ?? localVariableURLString), parameters: localVariableParameters, headers: localVariableHeaderParameters)
+    }
+
+    /**
+     Upload profile protocol diagnostic logs
+     
+     - parameter profileId: (path)  
+     - parameter protocolSlug: (path)  
+     - parameter apiResponseQueue: The queue on which api response is dispatched.
+     - parameter completion: completion handler to receive the data and the error objects
+     */
+    @discardableResult
+    open class func uploadProfileProtocolLogs(profileId: UUID, protocolSlug: UUID, apiResponseQueue: DispatchQueue = ForestVPNCloudSDKAPIAPI.apiResponseQueue, completion: @escaping ((_ data: String?, _ error: Error?) -> Void)) -> RequestTask {
+        return uploadProfileProtocolLogsWithRequestBuilder(profileId: profileId, protocolSlug: protocolSlug).execute(apiResponseQueue) { result in
+            switch result {
+            case let .success(response):
+                completion(response.body, nil)
+            case let .failure(error):
+                completion(nil, error)
+            }
+        }
+    }
+
+    /**
+     Upload profile protocol diagnostic logs
+     - POST /profiles/{profileId}/protocols/{protocolSlug}/logs/
+     - BASIC:
+       - type: http
+       - name: BearerTokenAuth
+     - parameter profileId: (path)  
+     - parameter protocolSlug: (path)  
+     - returns: RequestBuilder<String> 
+     */
+    open class func uploadProfileProtocolLogsWithRequestBuilder(profileId: UUID, protocolSlug: UUID) -> RequestBuilder<String> {
+        var localVariablePath = "/profiles/{profileId}/protocols/{protocolSlug}/logs/"
+        let profileIdPreEscape = "\(APIHelper.mapValueToPathItem(profileId))"
+        let profileIdPostEscape = profileIdPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+        localVariablePath = localVariablePath.replacingOccurrences(of: "{profileId}", with: profileIdPostEscape, options: .literal, range: nil)
+        let protocolSlugPreEscape = "\(APIHelper.mapValueToPathItem(protocolSlug))"
+        let protocolSlugPostEscape = protocolSlugPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+        localVariablePath = localVariablePath.replacingOccurrences(of: "{protocolSlug}", with: protocolSlugPostEscape, options: .literal, range: nil)
+        let localVariableURLString = ForestVPNCloudSDKAPIAPI.basePath + localVariablePath
+        let localVariableParameters: [String: Any]? = nil
+
+        let localVariableUrlComponents = URLComponents(string: localVariableURLString)
+
+        let localVariableNillableHeaders: [String: Any?] = [
+            :
+        ]
+
+        let localVariableHeaderParameters = APIHelper.rejectNilHeaders(localVariableNillableHeaders)
+
+        let localVariableRequestBuilder: RequestBuilder<String>.Type = ForestVPNCloudSDKAPIAPI.requestBuilderFactory.getBuilder()
+
+        return localVariableRequestBuilder.init(method: "POST", URLString: (localVariableUrlComponents?.string ?? localVariableURLString), parameters: localVariableParameters, headers: localVariableHeaderParameters)
     }
 }
--- docs/ProfilesAPI.md
+++ docs/ProfilesAPI.md
@@ -4,10 +4,62 @@ All URIs are relative to *https://api.forestvpn.com/cloud/v1*
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
+[**attachDeviceInfo**](ProfilesAPI.md#attachdeviceinfo) | **POST** /profiles/{profileId}/device/info/ | Attach device info
 [**getProfile**](ProfilesAPI.md#getprofile) | **GET** /profiles/{profileId}/ | Profile details
-[**getProfileProtocolConfig**](ProfilesAPI.md#getprofileprotocolconfig) | **GET** /profiles/{profileId}/protocols/{protocolId}.conf | Profile protocol config
+[**getProfileProtocolConfig**](ProfilesAPI.md#getprofileprotocolconfig) | **GET** /profiles/{profileId}/protocols/{protocolSlug}.conf | Profile protocol config
 [**getProfileProtocols**](ProfilesAPI.md#getprofileprotocols) | **GET** /profiles/{profileId}/protocols/ | Profile supported protocols
+[**uploadProfileProtocolLogs**](ProfilesAPI.md#uploadprofileprotocollogs) | **POST** /profiles/{profileId}/protocols/{protocolSlug}/logs/ | Upload profile protocol diagnostic logs
 
+
+# **attachDeviceInfo**
+```swift
+    open class func attachDeviceInfo(profileId: UUID, deviceInfo: DeviceInfo? = nil, completion: @escaping (_ data: String?, _ error: Error?) -> Void)
+```
+
+Attach device info
+
+### Example
+```swift
+// The following code samples are still beta. For any issue, please report via http://github.com/OpenAPITools/openapi-generator/issues/new
+import ForestVPNCloudSDKAPI
+
+let profileId = 987 // UUID | 
+let deviceInfo = DeviceInfo(type: DeviceType(), info: "TODO") // DeviceInfo |  (optional)
+
+// Attach device info
+ProfilesAPI.attachDeviceInfo(profileId: profileId, deviceInfo: deviceInfo) { (response, error) in
+    guard error == nil else {
+        print(error)
+        return
+    }
+
+    if (response) {
+        dump(response)
+    }
+}
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **profileId** | **UUID** |  | 
+ **deviceInfo** | [**DeviceInfo**](DeviceInfo.md) |  | [optional] 
+
+### Return type
+
+**String**
+
+### Authorization
+
+[BearerTokenAuth](../README.md#BearerTokenAuth)
+
+### HTTP request headers
+
+ - **Content-Type**: application/json
+ - **Accept**: text/plain, application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
 
 # **getProfile**
 ```swift
@@ -59,7 +111,7 @@ Name | Type | Description  | Notes
 
 # **getProfileProtocolConfig**
 ```swift
-    open class func getProfileProtocolConfig(profileId: UUID, protocolId: UUID, completion: @escaping (_ data: String?, _ error: Error?) -> Void)
+    open class func getProfileProtocolConfig(profileId: UUID, protocolSlug: UUID, completion: @escaping (_ data: String?, _ error: Error?) -> Void)
 ```
 
 Profile protocol config
@@ -70,10 +122,10 @@ Profile protocol config
 import ForestVPNCloudSDKAPI
 
 let profileId = 987 // UUID | 
-let protocolId = 987 // UUID | 
+let protocolSlug = 987 // UUID | 
 
 // Profile protocol config
-ProfilesAPI.getProfileProtocolConfig(profileId: profileId, protocolId: protocolId) { (response, error) in
+ProfilesAPI.getProfileProtocolConfig(profileId: profileId, protocolSlug: protocolSlug) { (response, error) in
     guard error == nil else {
         print(error)
         return
@@ -90,7 +142,7 @@ ProfilesAPI.getProfileProtocolConfig(profileId: profileId, protocolId: protocolI
 Name | Type | Description  | Notes
 ------------- | ------------- | ------------- | -------------
  **profileId** | **UUID** |  | 
- **protocolId** | **UUID** |  | 
+ **protocolSlug** | **UUID** |  | 
 
 ### Return type
 
@@ -152,6 +204,56 @@ Name | Type | Description  | Notes
 
  - **Content-Type**: Not defined
  - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
+# **uploadProfileProtocolLogs**
+```swift
+    open class func uploadProfileProtocolLogs(profileId: UUID, protocolSlug: UUID, completion: @escaping (_ data: String?, _ error: Error?) -> Void)
+```
+
+Upload profile protocol diagnostic logs
+
+### Example
+```swift
+// The following code samples are still beta. For any issue, please report via http://github.com/OpenAPITools/openapi-generator/issues/new
+import ForestVPNCloudSDKAPI
+
+let profileId = 987 // UUID | 
+let protocolSlug = 987 // UUID | 
+
+// Upload profile protocol diagnostic logs
+ProfilesAPI.uploadProfileProtocolLogs(profileId: profileId, protocolSlug: protocolSlug) { (response, error) in
+    guard error == nil else {
+        print(error)
+        return
+    }
+
+    if (response) {
+        dump(response)
+    }
+}
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **profileId** | **UUID** |  | 
+ **protocolSlug** | **UUID** |  | 
+
+### Return type
+
+**String**
+
+### Authorization
+
+[BearerTokenAuth](../README.md#BearerTokenAuth)
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
+ - **Accept**: text/plain, application/json
 
 [[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
 
